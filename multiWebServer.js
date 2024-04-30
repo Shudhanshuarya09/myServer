12463afc30a9f3f0ee42c2db1ddf047ed71be6fa
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-04-30 23:42:09 +0530

    fix

--- multiWebServer.js
+++ multiWebServer.js
@@ -1,10 +1,13 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
   const log = `${Date.now()}: ${req.url} New Req Received\n`
+  const myUrl = url.parse(req.url);
+  console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
     case "/":
       res.end("HomePage");
       break;
